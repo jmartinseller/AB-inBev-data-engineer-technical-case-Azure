d885d3223526ef40dc30a9ce571e510f37d48c2c
unknown <jmartinseller@gmail.com>
2025-04-22 18:27:14 -0300

    Criação de template do projeto

--- Databricks/Notebooks/gold_aggregation.py
+++ Databricks/Notebooks/gold_aggregation.py
@@ -1,14 +1,33 @@
 # Databricks notebook source
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import count
+from pyspark.sql.functions import count, col
 
-# Caminhos
-silver_path = "abfss://<seu-container>@<sua-conta>.dfs.core.windows.net/silver/"
-gold_path = "abfss://<seu-container>@<sua-conta>.dfs.core.windows.net/gold/"
+spark = SparkSession.builder \
+    .appName("AgregacaoCervejarias") \
+    .getOrCreate()
+    
+    
+# Ler os dados da tabela Delta silve
+df_cervejarias = spark.read.table("brewery_adf_v2.silver.silver_data")
 
-spark = SparkSession.builder.getOrCreate()
-df_silver = spark.read.parquet(silver_path)
-
-# Agregação: contagem por tipo e estado
-df_agg = df_silver.groupBy("brewery_type", "state").agg(count("*").alias("brewery_count"))
-df_agg.write.mode("overwrite").parquet(gold_path)
+# criar a agregações
+df_agregado = df_cervejarias.groupBy("brewery_type", "state", "city") \
+    .agg(count("*").alias("quantidade_cervejarias")) \
+    .orderBy(col("quantidade_cervejarias").desc(), "state", "city")
+    
+# salvando tabela Delta gold
+df_agregado.write \
+    .format("delta") \
+    .mode("overwrite") \
+    .saveAsTable("brewery_adf_v2.gold.gold_data")
+    
+#Criar uma view SQL para tabela gold
+spark.sql('''
+CREATE OR REPLACE VIEW brewery_adf_v2.gold.vw_cervejarias AS
+SELECT 
+    brewery_type as tipo_cervejaria,
+    state as estado,
+    city as cidade,
+    quantidade_cervejarias
+FROM brewery_adf_v2.gold.gold_data
+''')
